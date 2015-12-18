f365de5f565be4a556d7a58d6b4cfcaebaec6fb8
tsubo <ktsubota@gmail.com>
2015-12-18 19:55:33 +0900

    「ControllerとRouting」を完了

---
+++ src/AppBundle/Controller/OmikujiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class OmikujiController extends Controller
+{
+    /**
+     * おみくじ運勢を表示する
+     *
+     * @Route("/omikuji/{yourname}", defaults={"yourname" = "YOU"}, name="omikuji")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function omikujiAction(Request $request, $yourname)
+    {
+        $omikuji = ['大吉', '中吉', '小吉', '末吉', '凶'];
+        $number = rand(0, count($omikuji) - 1);
+
+        return new Response(
+            "<html><body>{$yourname}さんの運勢は $omikuji[$number] です。</body></html>"
+        );
+    }
+}
